3a84f0d2f4c0d35a81075fe6412b2ea58f6ea694
Igor <c.igor@gmx.com>
2015-10-01 15:44:38 +0200

    this thing should be disabled...

--- device.mk
+++ device.mk
@@ -197,7 +197,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.ril.config=qcomdsds,nodefaultdata
 
 # Upto 3 layers can go through overlays
-PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=true
+PRODUCT_PROPERTY_OVERRIDES += persist.hwc.mdpcomp.enable=false
 
 # More overrides from stock build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
