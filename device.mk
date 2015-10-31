85187b7bccd3841be5482027c37d07b246aa0556
Igor <ShadySquirrel@users.noreply.github.com>
2015-10-31 22:40:16 +0100

    Update additional bluetooth stuff from latest zombigrn's zip
    - move everything to bluetooth_extra directory,
    - update existing files in etc,
    - add needed framework file,
    - add needed libs,
    - add needed binary
    
    Again, big thanks to zombigrn@XDA, for all of his hard work and testing.

--- device.mk
+++ device.mk
@@ -53,22 +53,35 @@ PRODUCT_COPY_FILES += \
 
 # Add bluetooth configuration from stock
 PRODUCT_COPY_FILES += \
-       device/lge/e980/etc/bluetooth/audio.conf:system/etc/bluetooth/audio.conf \
-       device/lge/e980/etc/bluetooth/auto_pair_devlist.conf:system/etc/bluetooth/auto_pair_devlist.conf \
-       device/lge/e980/etc/bluetooth/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
-       device/lge/e980/etc/bluetooth/avrcp_toggle_white_list.conf:system/etc/bluetooth/avrcp_toggle_white_list.conf \
-       device/lge/e980/etc/bluetooth/blacklist.conf:system/etc/bluetooth/blacklist.conf \
-       device/lge/e980/etc/bluetooth/block_avrcp_toggle_new.conf:system/etc/bluetooth/block_avrcp_toggle_new.conf \
-       device/lge/e980/etc/bluetooth/bt.a2dp.tuning.sh:system/etc/bluetooth/bt.a2dp.tuning.sh \
-       device/lge/e980/etc/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
-       device/lge/e980/etc/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
-       device/lge/e980/etc/bluetooth/bt_tts_list.conf:system/etc/bluetooth/bt_tts_list.conf \
-       device/lge/e980/etc/bluetooth/carkit_cod_headsets.conf:system/etc/bluetooth/carkit_cod_headsets.conf \
-       device/lge/e980/etc/bluetooth/device_config.xml:system/etc/bluetooth/device_config.xml \
-       device/lge/e980/etc/bluetooth/input.conf:system/etc/bluetooth/input.conf \
-       device/lge/e980/etc/bluetooth/iop_bt.db:system/etc/bluetooth/iop_bt.db \
-       device/lge/e980/etc/bluetooth/main.conf:system/etc/bluetooth/main.conf \
-       device/lge/e980/etc/bluetooth/network.conf:system/etc/bluetooth/network.conf \
+       device/lge/e980/bluetooth_extras/etc/audio.conf:system/etc/bluetooth/audio.conf \
+       device/lge/e980/bluetooth_extras/etc/auto_pair_devlist.conf:system/etc/bluetooth/auto_pair_devlist.conf \
+       device/lge/e980/bluetooth_extras/etc/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
+       device/lge/e980/bluetooth_extras/etc/avrcp_toggle_white_list.conf:system/etc/bluetooth/avrcp_toggle_white_list.conf \
+       device/lge/e980/bluetooth_extras/etc/blacklist.conf:system/etc/bluetooth/blacklist.conf \
+       device/lge/e980/bluetooth_extras/etc/block_avrcp_toggle_new.conf:system/etc/bluetooth/block_avrcp_toggle_new.conf \
+       device/lge/e980/bluetooth_extras/etc/bt.a2dp.tuning.sh:system/etc/bluetooth/bt.a2dp.tuning.sh \
+       device/lge/e980/bluetooth_extras/etc/bt_did.conf:system/etc/bluetooth/bt_did.conf \
+       device/lge/e980/bluetooth_extras/etc/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
+       device/lge/e980/bluetooth_extras/etc/bt_tts_list.conf:system/etc/bluetooth/bt_tts_list.conf \
+       device/lge/e980/bluetooth_extras/etc/carkit_cod_headsets.conf:system/etc/bluetooth/carkit_cod_headsets.conf \
+       device/lge/e980/bluetooth_extras/etc/device_config.xml:system/etc/bluetooth/device_config.xml \
+       device/lge/e980/bluetooth_extras/etc/input.conf:system/etc/bluetooth/input.conf \
+       device/lge/e980/bluetooth_extras/etc/iop_bt.db:system/etc/bluetooth/iop_bt.db \
+       device/lge/e980/bluetooth_extras/etc/main.conf:system/etc/bluetooth/main.conf \
+       device/lge/e980/bluetooth_extras/etc/network.conf:system/etc/bluetooth/network.conf \
+
+# Add bluetooth framework file...
+PRODUCT_COPY_FILES += \
+       device/lge/e980/bluetooth_extras/framework/com.broadcom.bt.jar:system/framework/com.broadcom.bt.jar \
+
+# Add bluetooth libs and binaries. Those should be in vendor/lge/e980, but that one needs some updating...
+PRODUCT_COPY_FILES += \
+       device/lge/e980/bluetooth_extras/lib/libbluedroid_bdt.so:system/lib/libbluedroid_bdt.so \
+       device/lge/e980/bluetooth_extras/lib/libbluetooth_jni.so:system/lib/libbluetooth_jni.so \
+       device/lge/e980/bluetooth_extras/lib/libbluetoothsettings_jni.so:system/lib/libbluetoothsettings_jni.so \
+       device/lge/e980/bluetooth_extras/xbin/hciconfig:system/xbin/hciconfig \
+       device/lge/e980/bluetooth_extras/xbin/hcidump:system/xbin/hcidump \
+       device/lge/e980/bluetooth_extras/xbin/hcitool:system/xbin/hcitool \
 
 PRODUCT_COPY_FILES += \
        device/lge/e980/initlogo.rle:root/initlogo.rle888 \
