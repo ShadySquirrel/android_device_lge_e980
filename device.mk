eb523f9deafb9ae6bbb7aa6b2db087e36ee41c10
Igor C <shady.squirrel@caramail.com>
2017-08-25 22:35:23 +0200

    Revert "e980: remove dex2oat overrides"
    This reverts commit d40468d937d3c2e4d6eaf128b946afe94ab2e8b2.

--- device.mk
+++ device.mk
@@ -227,8 +227,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 ## OVERRIDES - should go into system.prop... but too lazy. ##
+# override dex2oat config
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=420
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.dex2oat-swap=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=400
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.bt.bdaddr_path=/data/misc/bdaddr
